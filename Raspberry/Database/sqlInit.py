3576489ac1a931e4d7854abb5db587d808113d84
danielnahum <daniel.nahum.guti@gmail.com>
2022-11-29 17:12:48 -0300

    intento de inicializacion de bd

--- Raspberry/Database/sqlInit.py
+++ Raspberry/Database/sqlInit.py
@@ -1,52 +1,79 @@
-import sqlite3 as sql
+import mysql.connector as sql
+# from mysql import Error
 
-def init_database():
-    create_data_table = \
-'''CREATE TABLE Datos(\
-IDDevice TEXT, \
-MAC TEXT, \
-TransportLayer INTEGER, \
-IDProtocol INTEGER, \
-Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, \
-Data1, Data2, Data3, \
-Data4, Data5, Data6, \
-Data7, Data8, Data9, \
-Data10, Data11, Data12, \
-Data13, Data14\
-);'''
+from pathlib import Path
 
-    create_logs_table = \
-'''CREATE TABLE Logs(\
-IDDevice TEXT, \
-MAC TEXT, \
-TransportLayer INTEGER, \
-IDProtocol INTEGER, \
-Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP\
-);'''
+folderPath = Path(__file__).parent
 
-    create_config_table = \
-'''CREATE TABLE Config(\
-IDProtocol INTEGER PRIMARY KEY, \
-TransportLayer INTEGER DEFAULT 0\
-);'''
+# def init_database():
+#     create_data_table = \
+# '''CREATE TABLE Datos(\
+# IDDevice TEXT, \
+# MAC TEXT, \
+# TransportLayer INTEGER, \
+# IDProtocol INTEGER, \
+# Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, \
+# Data1, Data2, Data3, \
+# Data4, Data5, Data6, \
+# Data7, Data8, Data9, \
+# Data10, Data11, Data12, \
+# Data13, Data14\
+# );'''
 
-    conn = sql.connect('DB.sqlite')
-    c = conn.cursor()
-    try:
-        c.execute(create_data_table)
-    except sql.OperationalError:
-        print("La tabla Datos ya existe")
-    try:
-        c.execute(create_logs_table)
-    except sql.OperationalError:
-        print("La tabla Logs ya existe")
-    try:
-        c.execute(create_config_table)
-    except sql.OperationalError:
-        print("La tabla Config ya existe")
+#     create_logs_table = \
+# '''CREATE TABLE Logs(\
+# IDDevice TEXT, \
+# MAC TEXT, \
+# TransportLayer INTEGER, \
+# IDProtocol INTEGER, \
+# Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP\
+# );'''
+
+#     create_config_table = \
+# '''CREATE TABLE Config(\
+# IDProtocol INTEGER PRIMARY KEY, \
+# TransportLayer INTEGER DEFAULT 0\
+# );'''
+
+#     conn = sql.connect('DB.sqlite')
+#     c = conn.cursor()
+#     try:
+#         c.execute(create_data_table)
+#     except sql.OperationalError:
+#         print("La tabla Datos ya existe")
+#     try:
+#         c.execute(create_logs_table)
+#     except sql.OperationalError:
+#         print("La tabla Logs ya existe")
+#     try:
+#         c.execute(create_config_table)
+#     except sql.OperationalError:
+#         print("La tabla Config ya existe")
     
-    conn.commit()
-    print("Tablas en la base de datos:")
-    print(c.execute("select name from sqlite_master;").fetchall())
+#     conn.commit()
+#     print("Tablas en la base de datos:")
+#     print(c.execute("select name from sqlite_master;").fetchall())
 
-    conn.close()
+#     conn.close()
+
+print("Starting...")
+cnn = None
+filename = folderPath / 'DB.mysql'
+
+
+with open((folderPath / "dbFile.sql").resolve(),"r") as sql_file:
+    sqlite = sql_file.read()
+
+cnn = sql.connect(
+    host="localhost",
+    user="rasppi",
+    password="tupungatitogaming"
+)
+print("database connected..")
+cs = cnn.cursor()
+cs.execute(sqlite)
+cnn.commit()
+
+if cnn:
+    cnn.close()
+print("done..")
