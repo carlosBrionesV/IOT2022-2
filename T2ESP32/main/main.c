dfa1f85a2572525b45802b1fda882b9b38546d53
DamnXitri <71194733+DamnXitri@users.noreply.github.com>
2022-12-04 02:03:55 -0300

    fixed build errors

--- T2ESP32/main/main.c
+++ T2ESP32/main/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+#include "almacenamiento.h"
+#include "configuracion.h"
+#include "empaquetamiento.h"
+#include "tcp_client.h"
+#include "udp_client.h"
+#include "wifi_client.h"
+
 void app_main(void)
 {
 
