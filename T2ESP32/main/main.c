844652b6e5da563a88c7a20cc756d4a064dd0135
DamnXitri <71194733+DamnXitri@users.noreply.github.com>
2022-12-05 01:25:43 -0300

    main program

--- T2ESP32/main/main.c
+++ T2ESP32/main/main.c
@@ -13,21 +13,74 @@
 #include "nvs_flash.h"
 
 #include "almacenamiento.h"
+#include "ble_server.h"
 #include "configuracion.h"
-#include "empaquetamiento.h"
 #include "tcp_client.h"
 #include "udp_client.h"
 #include "wifi_client.h"
-#include "ble_server.h"
+
+
+#define TAG "MAIN"
 
 void app_main(void)
 {
 
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     // Inicializar el sistema de archivos
     initNVS();
 
-    xTaskCreate(bleConfig, "bleConfig", 4096, NULL, 5, NULL);
+    resetConfig();
+    int configurated = readConfigurated();
+    printConfig(readConfig());
+    if (!configurated)
+    {
+        bleConfig();
+    }
+
+    while (1)
+    {
+        T2_CONFIG config = readConfig();
+
+        if (20 <= config.STATUS && config.STATUS <= 23)
+        {
+            wifi_connect(config.SSID, config.PASS, WIFI_AUTH_WPA2_PSK);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+            switch (config.STATUS)
+            {
+            case 20:
+                configurateTcp();
+                break;
+            case 23:
+                manageUdp();
+                break;
+            default:
+                manageTcp();
+                break;
+            }
+
+            wifi_stop();
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+        else
+        {
+            switch (config.STATUS)
+            {
+            case 0:
+                bleConfig();
+                break;
+            case 30:
+                ESP_LOGI(TAG, "BLE CONTINUOUS NOT IMPLEMENTED");
+                break;
+            case 31:
+                ESP_LOGI(TAG, "BLE DISCONTINUOUS NOT IMPLEMENTED");
+                break;
+            default:
+                ESP_LOGI(TAG, "INVALID STATUS, reseting configuration");
+                resetConfig();
+                break;
+            }
+        }
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
